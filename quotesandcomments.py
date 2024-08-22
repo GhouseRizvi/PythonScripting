2a051879b8c5e3872ebd69b37f4820eb2f14c0a6
GhouseRizvi <ghouserizvi@gmail.com>
2024-08-22 23:36:15 +0530

    Initial commit learning python scripting

---
+++ quotesandcomments.py
@@ -0,0 +1,14 @@
+# This is a single line comment
+"""
+This is a
+multi
+line comment
+"""
+skill = "Devops"
+print(skill)
+print("String in double quote")
+print('String is single quote')
+print("""Multi line Paragraph String
+Here you can write any amount of test
+in multiple line
+""")
---
+++ sapmle-script.py
@@ -0,0 +1,10 @@
+x=2
+if x==0:
+    print("Executing if block")
+    print("Value of x is ",x)
+else:
+    print("Executing else block")
+    print("Value of x is ",x)
+
+print()
+print(" Out of if/else Block")
